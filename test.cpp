62567d81ddd2fd71a0229e03afb83ea7c1460bef
tsakalerisk <13333629+tsakalerisk@users.noreply.github.com>
2019-12-07 11:38:51 +0200

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include "SDL.h"
+
+int main(int agrc, char *args[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    bool quit = false;
+    while (!quit) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+                break;
+            }
+        }
+        if (quit) break;
+        SDL_SetRenderDrawColor(renderer, 0xff, 0, 0, 0xff);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+    SDL_Quit();
+    return 0;
+}
