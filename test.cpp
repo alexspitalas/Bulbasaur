dc290dd8c5a9b76c6d3f39b33610efb3258f3d48
tsakalerisk <13333629+tsakalerisk@users.noreply.github.com>
2019-12-07 11:45:25 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,5 +1,5 @@
 #include "SDL.h"
-
+//https://lazyfoo.net/tutorials/SDL/06_extension_libraries_and_loading_other_image_formats/linux/index.php
 int main(int agrc, char *args[]) {
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* window = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
